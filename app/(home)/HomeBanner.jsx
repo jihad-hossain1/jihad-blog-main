e8d443e8841aff359b5cc1753f1d3216f9b430e3
jihad-hossain1 <jihadkhan934@gmail.com>
2023-12-25 21:03:23 +0600

    some change on about botton

--- app/(home)/HomeBanner.jsx
+++ app/(home)/HomeBanner.jsx
@@ -55,8 +55,11 @@ const HomeBanner = () => {
                   Specialized in MERN Stack Developer.
                 </div>
               </div>
-              <div className="relative mt-7 md:mt-10 flex justify-center ">
-                <Link href={"/about"} className="btn w-fit absolute z-20">
+              <div className="relative mt-7 md:mt-10 flex justify-center mb-4">
+                <Link
+                  href={"/about"}
+                  className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover: translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none w-fit absolute z-20"
+                >
                   About Me
                 </Link>
               </div>
--- app/blogsArticle/loading.jsx
+++ app/blogsArticle/loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh)]">
-      <h4>Loading......</h4>
+      Loading......
     </div>
   );
 };
