3f15de63cc01e4185286bf61ba60cb5a40d7a84b
jihad-hossain1 <jihadkhan934@gmail.com>
2024-01-22 00:08:17 +0600

    aabout page added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import About from "./About";
 
 const AboutPage = () => {
   return (
     <main className="min-h-screen max-w-screen-xl mx-auto px-2 py-4">
-      <h4>AboutPage</h4>
-      <p>In progress....</p>
+      <About />
     </main>
   );
 };
