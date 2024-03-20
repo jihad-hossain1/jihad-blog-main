acbaca4384c1304a9f3fae47cb329bc4bbd16ca3
jihad-hossain1 <jihadkhan934@gmail.com>
2024-03-20 20:08:30 +0600

    some chnage form user

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,10 +7,17 @@ const ProfilePage = () => {
   const { data: session } = useSession();
   return (
     <MainContainer>
-      <div className="bg-green-100 shadow-xl p-4 w-fit flex flex-col items-center">
-        <h4>name: {session?.user?.name}</h4>
-        <h4>email: {session?.user?.email}</h4>
-        <h4>email: {session?.user?.role}</h4>
+      <div className="flex justify-center items-center flex-col min-h-[70vh]">
+        <div className="bg-white shadow-[2px_5px_15px_rgba(0,0,0,0.25)] rounded-md p-4 ">
+          <h4 className="uppercase">name: {session?.user?.name}</h4>
+          <h4 className="uppercase">email: {session?.user?.email}</h4>
+          <h4 className="uppercase ">
+            user-role:{" "}
+            <span className="text-green-600 underline">
+              {session?.user?.role}
+            </span>
+          </h4>
+        </div>
       </div>
     </MainContainer>
   );
