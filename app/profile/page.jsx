c2c72aaf611b0c05e2a84f22bd54dbcdd7686f33
jihad-hossain1 <jihadkhan934@gmail.com>
2024-03-20 19:42:59 +0600

    some chnage for user role

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -10,6 +10,7 @@ const ProfilePage = () => {
       <div className="bg-green-100 shadow-xl p-4 w-fit flex flex-col items-center">
         <h4>name: {session?.user?.name}</h4>
         <h4>email: {session?.user?.email}</h4>
+        <h4>email: {session?.user?.role}</h4>
       </div>
     </MainContainer>
   );
