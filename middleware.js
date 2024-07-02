fdc5b4f63b274de5d2491328ad6a82ea5de117f4
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-07-03 02:46:30 +0600

    middleware ch

--- middleware.js
+++ middleware.js
@@ -2,21 +2,22 @@ import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 
 
-export default withAuth(
-  async function middleware(req) {
-    if (
-      req.nextUrl.pathname.startsWith("/dashboard") &&
-      req.nextauth.token.role != "admin"
-    ) {
-      return NextResponse.rewrite(new URL("/denied", req.url));
-    }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
+// export default withAuth(
+//   async function middleware(req) {
+//     console.log(req);
+//     if (
+//       req.nextUrl.pathname.startsWith("/dashboard") &&
+//       req.nextauth.token.role != "admin"
+//     ) {
+//       return NextResponse.rewrite(new URL("/denied", req.url));
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => !!token,
+//     },
+//   }
+// );
 
 export const config = { matcher: ["/dashboard/:path*", "/profile"] };
 
@@ -24,13 +25,13 @@ export const config = { matcher: ["/dashboard/:path*", "/profile"] };
 
 
 
-// export async function middleware(request) {
-//   // const authUser = await serverAuth();
-//   const isT = false;
+export async function middleware(request) {
+  // const authUser = await serverAuth();
+  const isT = true;
 
-//   if (isT) {
-//     return NextResponse.next();
-//   }
-//   const loginUrl = new URL("/denied", request.url);
-//   return NextResponse.redirect(loginUrl);
-// }
+  if (isT) {
+    return NextResponse.next();
+  }
+  const loginUrl = new URL("/denied", request.url);
+  return NextResponse.redirect(loginUrl);
+}
