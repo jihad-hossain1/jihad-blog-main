c570c87dcc7015ed8c1e401cb96fbcf7bc508be4
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-07-03 21:10:12 +0600

    error fix

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ export const config = { matcher: ["/dashboard/:path*", "/profile"] };
 
 export async function middleware(request) {
   const sessionData = await getCookieData(request);
-  const loginUrl = new URL("/denied", request.url);
+  const loginUrl = new URL("/login", request.url);
   const deniedUrl = new URL("/denied", request.url);
 
   if (
@@ -16,12 +16,11 @@ export async function middleware(request) {
     sessionData?.role !== "admin"
   ) {
     return NextResponse.redirect(deniedUrl);
-  } else if (sessionData === null) {
-    return NextResponse.redirect(loginUrl);
-  } else {
-    return NextResponse.next();
   }
-  
+  if (request.nextUrl.pathname.startsWith("/profile") && sessionData === null) {
+    return NextResponse.redirect(loginUrl);
+  }
+  return NextResponse.next();
   
 }
 
