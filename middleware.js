32dfa9c6dc6c2450f0b01e6a97f0448b8b6d4fa5
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-07-03 22:28:12 +0600

    some 53434

--- middleware.js
+++ middleware.js
@@ -1,44 +1,66 @@
 import { NextResponse } from "next/server";
-import { getCookieData } from "./utils/fetch/session/fetchSession";
+import { withAuth } from "next-auth/middleware";
 
-function includes(collection, value) {
-  if (Array.isArray(collection)) {
-    for (let i = 0; i < collection.length; i++) {
-      if (collection[i] === value) {
-        return true;
-      }
+
+export default withAuth(
+  async function middleware(req) {
+    
+    if (
+      req.nextUrl.pathname.startsWith("/dashboard") &&
+      req.nextauth.token.role != "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", req.url));
     }
-    return false;
-  } else if (typeof collection === "string") {
-    return collection.indexOf(value) !== -1;
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
   }
-  return false;
-}
+);
 
-const isAdminRoute = (pathname) => {
-  return pathname.startsWith("/dashboard");
-};
+export const config = { matcher: ["/dashboard/:path*"] };
 
-const isUserRoute = (pathname) => {
-  return pathname.startsWith("/profile");
-};
+// import { NextResponse } from "next/server";
+// import { getCookieData } from "./utils/fetch/session/fetchSession";
 
-export async function middleware(req) {
-  const sessionData = await getCookieData();
-  const role = await sessionData?.role;
-  console.log("🚀 ~ middleware ~ role:", role);
+// function includes(collection, value) {
+//   if (Array.isArray(collection)) {
+//     for (let i = 0; i < collection.length; i++) {
+//       if (collection[i] === value) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   } else if (typeof collection === "string") {
+//     return collection.indexOf(value) !== -1;
+//   }
+//   return false;
+// }
 
-  const { pathname } = req.nextUrl;
+// const isAdminRoute = (pathname) => {
+//   return pathname.startsWith("/dashboard");
+// };
 
-  if (isUserRoute(pathname) && !includes(["user", "admin"], role)) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+// const isUserRoute = (pathname) => {
+//   return pathname.startsWith("/profile");
+// };
 
-  if (isAdminRoute(pathname) && role !== "admin") {
-    return NextResponse.redirect(new URL("/denied", req.url));
-  }
+// export async function middleware(req) {
+//   const sessionData = await getCookieData();
+//   const role = await sessionData?.role;
 
-  return NextResponse.next();
-}
+//   const { pathname } = req.nextUrl;
 
-export const config = { matcher: ["/dashboard/:path*", "/profile"] };
+//   if (isUserRoute(pathname) && !includes(["user", "admin"], role)) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+
+//   if (isAdminRoute(pathname) && role !== "admin") {
+//     return NextResponse.redirect(new URL("/denied", req.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = { matcher: ["/dashboard/:path*", "/profile"] };
