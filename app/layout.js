0889f1ff7d2466450b6c2602c0a3d5397137733b
jihad hossain <jihadkhan934@gmail.com>
2024-09-04 00:19:26 +0600

    static meta data generate

--- app/layout.js
+++ app/layout.js
@@ -1,37 +1,64 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
 import NavbarSmall from "@/components/Navbar/NavbarSmall";
 import Footer from "@/components/Footer/Footer";
 import { NextAuthProvider } from "./Provider";
 import { Toaster } from "@/components/ui/toaster";
 import HotToaster from "@/components/Toaster/HotToaster";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Jihad Blogs",
-  description: "Share Your Programming Knowledge",
+    title: "Jihad Blogs",
+    description: "Share Your Programming Knowledge",
+    keywords:
+        "programming, blog, programming, programming blog, programming knowledge, programming blog, programming knowledge, programming blog, programming knowledge, programming blog, programming knowledge",
+    openGraph: {
+        title: "Jihad Blogs",
+        description: "Share Your Programming Knowledge",
+        url: "https://jihad-blog-main.vercel.app",
+        images: [],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Jihad Blogs",
+        description: "Share Your Programming Knowledge",
+        images: [],
+    },
+
+    icons: {
+        images: [],
+    },
+
+    themeColor: [],
+    metadata: {
+        description: "Jihad Blogs",
+    },
+
+    viewport: {
+        width: "device-width",
+        initialScale: 1,
+        maximumScale: 1,
+    },
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" data-theme="dark">
-      <body className={inter.className}>
-          <NextAuthProvider>
-            <div className="flex min-h-screen flex-col justify-between">
-              <div>
-                <NavbarSmall />
-                <main className="bg-gray-100">{children}</main>
-              </div>
-            </div>
-            <Footer />
-          </NextAuthProvider>
-          <Toaster />
-          <HotToaster />
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en' data-theme='dark'>
+            <body className={inter.className}>
+                <NextAuthProvider>
+                    <div className='flex min-h-screen flex-col justify-between'>
+                        <div>
+                            <NavbarSmall />
+                            <main className='bg-gray-100'>{children}</main>
+                        </div>
+                    </div>
+                    <Footer />
+                </NextAuthProvider>
+                <Toaster />
+                <HotToaster />
+            </body>
+        </html>
+    );
 }
